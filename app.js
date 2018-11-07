57c69fa5edfad4c5cb49a219c4d3742827e2d3d9
Eli Silver <esilver17@gmail.com>
2018-11-06 19:36:33 -0500

    4

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ var app = express();
 
 // Routes
 app.get('/', function(req, res) {
+  res.type('text');   
+
   if (req.query.q == "Phone"){
     res.send('9179249076');
   }
   if (req.query.q == "Puzzle"){
-    res.send('&nbsp' + 'ABCD' +'</br>'
-    +'A=>>>' +'</br>'
-    +'B<=<<' +'</br>'
-    +'C<>=>' +'</br>'
+    res.send(' ABCD\n'
+    +'A=>>>\n'
+    +'B<=<<\n'
+    +'C<>=>\n'
     +'D<><=');
   }
   res.send('OK');
